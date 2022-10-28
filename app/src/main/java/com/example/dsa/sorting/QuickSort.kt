9c83119609d469dedc461881e8f182bd60dbce61
vishwanath <vishwanath.gugwad@gmail.com>
2022-10-28 10:35:25 +0530

    Quicksort

--- app/src/main/java/com/example/dsa/sorting/QuickSort.kt
+++ app/src/main/java/com/example/dsa/sorting/QuickSort.kt
@@ -5,23 +5,29 @@ import com.example.dsa.sorting.QuickSort.Companion.printArray
 class QuickSort {
 
     companion object {
-        fun partition(arr: Array<Int>, start: Int, end: Int): Int {
-            val pivot = end
+        private fun partition(arr: Array<Int>, start: Int, end: Int): Int {
+            val pivot = arr[end]
             var i = start - 1
             for (j in start until end) {
-                if (arr[j] <= arr[pivot]) {
+                if (arr[j] <= pivot) {
                     i++
-                    var temp = arr[i]
+                    //swap
+                    val temp = arr[i]
                     arr[i] = arr[j]
                     arr[j] = temp
                 }
             }
+            //swap with pivot
+            i++
+            val temp = arr[i]
+            arr[i] = pivot
+            arr[end] = temp
             return i
         }
 
         fun quickSort(arr: Array<Int>, start: Int, end: Int) {
             if (start < end) {
-                var pivot = partition(arr, start, end)
+                val pivot = partition(arr, start, end)
                 quickSort(arr, start, pivot - 1)
                 quickSort(arr, pivot + 1, end)
             }
@@ -41,7 +47,7 @@ class QuickSort {
 }
 
 fun main(){
-    val arr = arrayOf(10,3,2,7,7,5,8,4,1,2,9,7,8,11)
+    val arr = arrayOf(10,3,2,7,5,8,4,1,9,11)
     QuickSort.quickSort(arr,0,arr.size-1)
     printArray(arr)
 
